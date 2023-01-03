cdea974d07343d4a78f6c6b540e49956704cf013
Student <league@iMac-18.attlocal.net>
2023-01-02 16:30:21 -0800

    update

--- src/_02_Chat_Application/ChatApp.java
+++ src/_02_Chat_Application/ChatApp.java
@@ -1,5 +1,12 @@
 package _02_Chat_Application;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import javax.swing.JOptionPane;
+
 /*
  * Using the Click_Chat example, write an application that allows a server computer to chat with a client computer.
  */
@@ -11,6 +18,41 @@ public class ChatApp {
 	}
 	
 	public void run() {
+		String mode = JOptionPane.showInputDialog("Are you the client or server? [C/s]");
+		if (mode.equalsIgnoreCase("s")){
+			server();
+		}else {
+			client();
+		}
+	}
+	
+	public void server(){
+		try {
+			ServerSocket server = new ServerSocket(8080);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void client() {
+		String ip = JOptionPane.showInputDialog("Server IP");
+		int port = Integer.parseInt(JOptionPane.showInputDialog("Port #"));
+		
+		try {
+			Socket socket = new Socket(ip, port);
+			write(socket);
+			recieve(socket);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void write(Socket socket) {
+		
+	}
+	
+	public void recieve(Socket socket) {
 		
 	}
 }
